1de3ad839904660d6243f9f367e977b1af768626
Shubham Kaura <shubhamkaura8@gmail.com>
2024-01-24 11:57:44 -0700

    Practice fetch & then code

--- Asynchronous JavaScript/script.js
+++ Asynchronous JavaScript/script.js
@@ -4,3 +4,36 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const renderCountry = function (data) {
+  const html = `<article class="country">
+    <img class="country__img" src="${data.flags.svg}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name.official}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${
+        Object.values(data.languages)[0]
+      }</p>
+      <p class="country__row"><span>💰</span>${
+        Object.values(data.currencies)[0].name
+      }</p>
+    </div>
+  </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data[0]);
+      renderCountry(data[0]);
+    });
+};
+
+getCountryData('portugal');
